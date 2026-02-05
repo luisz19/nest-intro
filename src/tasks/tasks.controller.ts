487a7059edbdc4cd01146be98b1cc0defa72e824
Luis Henrique <luiss18.code@gmail.com>
2026-02-05 11:54:44 -0300

    feat: adicionar módulo e controlador de tarefas

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Param } from '@nestjs/common';
+
+@Controller('tasks')
+export class TasksController {
+
+    @Get()
+    public findAll(): string[] {
+        return ['A', 'B']
+    }
+
+    @Get('/:id')
+    public findOne(@Param('id') id: string): string {
+        return `the number is ${id}`
+    }
+}
