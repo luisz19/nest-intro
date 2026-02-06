33aa89e559103db81fce7498816c3cef2242b9b1
Luis Henrique <luiss18.code@gmail.com>
2026-02-06 12:07:21 -0300

    feat: implementar serviço e modelo de tarefas com controlador atualizado

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,15 +1,33 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import type { ITask } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
 
+    constructor(private readonly tasksService: TasksService ) {}
+
     @Get()
-    public findAll(): string[] {
-        return ['A', 'B']
+    public findAll(): ITask[] {
+        return this.tasksService.findAll()
     }
 
     @Get('/:id')
-    public findOne(@Param('id') id: string): string {
-        return `the number is ${id}`
+    public findOne(@Param('id') id: string): ITask {
+        const task = this.tasksService.findOne(id)
+
+        if(task) {
+            return task
+        }
+
+        throw new NotFoundException()
+
+        //another way
+        // throw new HttpException({
+        //     status: HttpStatus.NOT_FOUND,
+        //     error: 'It is not possible to find that.',
+        // }, HttpStatus.NOT_FOUND, {
+        //     cause: 'something caused that'
+        // })
     }
 }
