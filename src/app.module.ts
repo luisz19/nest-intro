a5df3f9ccb9befe643993060058f6883c07b1b85
Luis Henrique <luiss18.code@gmail.com>
2026-03-06 18:32:48 -0300

    feat: adicionar configuração de variável de ambiente com validações com Joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,24 @@ import { AppService } from './app.service';
 import { MessageFormatterService } from './message-formatter/message-formatter.service';
 import { LoggerService } from './logger/logger.service';
 import { TasksModule } from './tasks/tasks.module';
+import { ConfigModule } from '@nestjs/config';
+import { appConfig } from './config/app.config';
+import { appConfigSchema } from './config/config.types';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [appConfig], //carrega antes
+      validationSchema: appConfigSchema,
+      validationOptions: {
+        // allowUnknown: false, só faz sentido se a aplicação estiver usando um container docker muito isolado que execute apenas Node por exemplo.
+        abortEarly: true, // alerta sobre possíveis erros com validação de Schema
+      },
+      
+    }),
+    
+    TasksModule
+  ],
   controllers: [AppController],
   providers: [AppService, MessageFormatterService, LoggerService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { LoggerService } from './logger/logger.service';
+import { ConfigService } from '@nestjs/config';
+import { ConfigType } from './config/config.types';
+import { AppConfig } from './config/app.config';
 
 @Injectable()
 export class AppService {
-  constructor(private readonly Logger: LoggerService ) {}
+  constructor(
+    private readonly Logger: LoggerService,
+    private readonly configService: ConfigService<ConfigType>
+  ) {}
 
   getHello(): string {
-    return this.Logger.log('Hello world');
+    const prefix = this.configService.get<AppConfig>('app')
+    return this.Logger.log(`${prefix} Hello World`);
   }
 }
